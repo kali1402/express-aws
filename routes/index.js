bbc16bf3fd80d28873df079e723c623137f15558
kali1402 <amcopeew68@naver.com>
2020-10-30 16:30:00 +0900

    메인 페이지 변경

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '삼일상업고등학교' });
 });
 
 module.exports = router;
