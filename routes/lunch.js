dac4640c5c541212ee5ac662d6ce8b00fe95e001
kali1402 <amcopeew68@naver.com>
2020-10-30 14:50:29 +0900

    api 키 숨기고 로그 지움

--- routes/lunch.js
+++ routes/lunch.js
@@ -2,18 +2,25 @@ const { default: Axios } = require('axios');
 var express = require('express');
 var router = express.Router();
 
+// 시간
+require('date-utils');
+var newDate = new Date();
+var time = newDate.toFormat('YYYYMMDD');
+
+// env 파일 키
+const key = require('dotenv').config().parsed.key;
+
 router.get('/', function (req, res, next) {
     let menu = [];
-    Axios.get('https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=5eba093627bc4dcb834f1b544816ef34&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=20201029')
+    Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${key}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=${time}`)
         .then(res => {
             const test = res.data.mealServiceDietInfo[1].row[0].DDISH_NM;
             for (let i = 0; i < test.split("<br/>").length; i++) {
                 menu[i] = test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join("");
-                console.log(menu[i]);
             }
         })
         .then(() => {
-            res.render('index', { data: menu, title: "점심메뉴" })
+            res.render('lunch', { data: menu, title: "점심메뉴" })
         })
 });
 module.exports = router;
