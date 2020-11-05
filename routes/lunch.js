68511690f24baf2006e90f6b430975f1241416ad
kali1402 <amcopeew68@naver.com>
2020-11-05 16:43:38 +0900

    정규식 수정

--- routes/lunch.js
+++ routes/lunch.js
@@ -63,8 +63,9 @@ router.get('/', function (req, res, next) {
     Axios.get(`https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${key}&Type=json&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7530336&MLSV_YMD=${time}`)
         .then(res => {
             const test = res.data.mealServiceDietInfo[1].row[0].DDISH_NM;
-            for (let i = 0; i < test.split("<br/>").length; i++) {
-                menu[i] = test.split("<br/>")[i].match(/[^(삼일)]/g).join('').match(/[가-힣]/g).join("");
+            for (let i = 0; i < test.split("br").length; i++) {
+                menu[i] = test.split("br")[i].match(/[가-힣]*(?=\(삼일\))|[가-힣]*(?=<)|[가-힣]*(?=[0-9])/g);
+                menu[i] = menu[i][0];
             }
         })
         .then(() => {
